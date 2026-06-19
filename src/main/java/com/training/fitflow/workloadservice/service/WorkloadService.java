2379e9eae04b7aecb6a062e0e02476c6f1af0125
Eugene Tsasiuk <gtsasuk@gmail.com>
2026-06-20 01:44:16 +0300

    Add updating workload method in service

--- src/main/java/com/training/fitflow/workloadservice/service/WorkloadService.java
+++ src/main/java/com/training/fitflow/workloadservice/service/WorkloadService.java
@@ -1,13 +1,47 @@
 package com.training.fitflow.workloadservice.service;
 
+import com.training.fitflow.workloadservice.dto.request.TrainerWorkloadRequest;
+import com.training.fitflow.workloadservice.model.ActionType;
+import com.training.fitflow.workloadservice.model.TrainerWorkloadSummary;
 import com.training.fitflow.workloadservice.storage.WorkloadStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
 public class WorkloadService {
     private final WorkloadStorage storage;
+
+    private TrainerWorkloadSummary createNew(TrainerWorkloadRequest request) {
+        TrainerWorkloadSummary summary = new TrainerWorkloadSummary();
+        summary.setUsername(request.trainerUsername());
+        summary.setFirstName(request.trainerFirstName());
+        summary.setLastName(request.trainerLastName());
+        summary.setActive(request.isActive());
+        return summary;
+    }
+
+    public void processWorkloadChange(TrainerWorkloadRequest request) {
+        String username = request.trainerUsername();
+        TrainerWorkloadSummary summary = storage.getStorage()
+                .computeIfAbsent(username, k -> createNew(request));
+
+        int year = request.trainingDate().getYear();
+        int month = request.trainingDate().getMonthValue();
+
+        summary.getYearMonthDuration()
+                .computeIfAbsent(year, k -> new HashMap<>());
+
+        if (request.actionType() == ActionType.ADD) {
+            summary.getYearMonthDuration().get(year)
+                    .merge(month, request.trainingDuration(), Long::sum);
+        } else {
+            summary.getYearMonthDuration().get(year)
+                    .merge(month, request.trainingDuration(), (current, delta) -> Math.max(0, current - delta));
+        }
+    }
 
 
 }
